822350672181fa32898b17547c69a70454c66fed
uniweb <uniweb@inbox.ru>
2019-12-10 22:03:54 +0500

    + детальные страницы книг,авторов,экземпляров

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,3 +1,5 @@
+var async = require('async');
+var Book = require('../models/book');
 var Author = require('../models/author');
 
 // Показать список всех авторов.
@@ -14,8 +16,28 @@ exports.author_list = function(req, res, next) {
   };
 
 // Показать подробную страницу для данного автора.
-exports.author_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
+exports.author_detail = function(req, res, next) {
+
+    async.parallel({
+        author: function(callback) {
+            Author.findById(req.params.id)
+              .exec(callback)
+        },
+        authors_books: function(callback) {
+          Book.find({ 'author': req.params.id },'title summary')
+          .exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); } // Error in API usage.
+        if (results.author==null) { // No results.
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
+        // Successful, so render.
+        res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books } );
+    });
+
 };
 
 // Показать форму создания автора по запросу GET.
